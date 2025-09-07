5bcf0d93c5a6549fdba14a21df871327a4547093
TalalJD <120325175+TalalJD@users.noreply.github.com>
2025-09-07 14:16:33 -0400

    Ajout de l'etat d'air du joueur

---
+++ CodeBreaker/Assets/Scripts/Player/FSM/AirState.cs
@@ -0,0 +1,54 @@
+using CodeBreaker;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirState : PlayerState
+{
+    public bool isJump;
+    
+    /// <summary>
+    /// Gere le movement du joueur quand il est dans les aires en appliquant la gravite. 
+    /// permet aussi de varier la force du saut dependant que quand le joueur relache la touche espace
+    /// </summary>
+    public void AirMovement()
+    {
+        if (isJump && Player.YSpeed > PhysicsInfo.JumpCutoff && Input.GetKeyUp(KeyCode.Space)) 
+        {
+            Player.YSpeed = PhysicsInfo.JumpCutoff; 
+        }
+
+        Player.YSpeed = Player.YSpeed - PhysicsInfo.Gravity*Time.fixedDeltaTime;
+
+    }
+    public override void OnEnter()
+    {
+
+    }
+
+    public override void OnExit()
+    {
+        isJump = false;
+        
+    }
+    public override void OnUpdate()
+    {
+
+    }
+
+    public override void OnFixedUpdate()
+    {
+        AirMovement();
+
+        //on verifie si le joueur a fini de jump
+        if (Player.YSpeed < 0)
+        {
+            if (Player.CheckOnGround())
+            {
+                Machine.Set<MoveState>();
+            }
+        }
+        
+    }
+
+}
