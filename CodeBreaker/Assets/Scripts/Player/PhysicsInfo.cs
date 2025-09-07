bf3f2bbdde83e19a4b2237da02726398474b2c80
TalalJD <120325175+TalalJD@users.noreply.github.com>
2025-09-07 13:49:25 -0400

    Ajout du scriptable object des valeurs pour les physiques custom du joueur

---
+++ CodeBreaker/Assets/Scripts/Player/PhysicsInfo.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class PhysicsInfo : ScriptableObject
+{
+
+    [Header("Player Physics")]
+    public float Acceleration = 20f;
+    public float Deceleration = 100f;
+    public float Friction = 40f;
+    public float MaxSpeed = 9.7f;
+    public float TopSpeed = 15f;
+
+    public float AirAcceleration = 15f;
+    public float AirDeceleration = 80f;
+    public float AirDrag = 10f;
+    public int JumpAmount = 2;
+    public float Gravity = 20f;
+    public float JumpStrength = 12.5f;
+    public float DoubleJumpMultiplier = 0.6f;
+    public float JumpCutoff = 4f;
+    public float MaxFallSpeed = 20f;
+    public float CoyoteTime = 0.15f;
+
+}
