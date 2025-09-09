25b3bef0c3cd49060edf4f59be735c2a4e6b99f4
YaniAgoune <ravless21@gmail.com>
2025-09-09 09:54:06 -0400

    Ajout de la méthode abstraite pour les armes

---
+++ CodeBreaker/Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+    [SerializeField] protected WeaponInfo _weaponInfo;
+    [SerializeField] private Transform firingPoint;
+    private float attackCooldown;
+
+
+    private void Start()
+    {
+        _weaponInfo = GetComponent<WeaponInfo>();
+        attackCooldown = 0f;
+    }
+    public abstract void Attack();
+
+    protected virtual void Update()
+    {
+        if (attackCooldown > 0f)
+        {
+            attackCooldown -= Time.deltaTime;
+        }
+    }
+
+    public virtual bool CanAttack()
+    {
+        return attackCooldown <= 0f;
+    }
+
+    protected virtual void ResetCooldown()
+    {
+        attackCooldown = _weaponInfo.attackSpeed;
+    }
+}
