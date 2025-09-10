7c3d90d9a4168b963fcd5b35e2bf0029c03fdb25
Vincentmdtran <2287365@bdeb.qc.ca>
2025-09-09 20:40:54 -0400

    Ajout boutons pour lancer/quitter du jeu

--- CodeBreaker/Assets/Scripts/MenuManager.cs
+++ CodeBreaker/Assets/Scripts/MenuManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CommencerJeu()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitterJeu()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
