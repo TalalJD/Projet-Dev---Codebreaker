1efebdba6c4f39fe62b893f86cebae82192a45d9
YaniAgoune <ravless21@gmail.com>
2025-09-15 10:10:03 -0400

    Ajout du scriptable object pour les ennemis

---
+++ CodeBreaker/Assets/Scripts/Ennemy/EnnemyInfo.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ennemy Object", menuName = "EnnemySystem/EnnemyObject")]
+public class EnnemyInfo : ScriptableObject
+{
+    public Sprite ennemySprite;
+    public GameObject ennemyPrefab;
+    public float movementSpeed;
+    public float healthPoints;
+    public float attackSpeed;
+    public float attackDamage;
+}
