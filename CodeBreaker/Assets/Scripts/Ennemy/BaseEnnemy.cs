040a0c1aa8ed6b548b9c45a3b5edbf4e39949106
YaniAgoune <ravless21@gmail.com>
2025-09-22 10:50:23 -0400

    Changement de la logique de mouvement pour l'ennemi de base

--- CodeBreaker/Assets/Scripts/Ennemy/BaseEnnemy.cs
+++ CodeBreaker/Assets/Scripts/Ennemy/BaseEnnemy.cs
@@ -4,26 +4,66 @@ using UnityEngine;
 
 public class BaseEnnemy : Ennemy
 {
+    private float moveSpeed = 10f;
+    private float gravity = -30f;
+    private float maxFallSpeed = -20f;
 
+    [SerializeField] private LayerMask LayerMask;
+    private float groundCheck = 0.55f;
+
+    private Collider2D _col;
+    private Vector2 _velocity;
+    private bool _isGrounded;
+
+
+    protected override void Start()
+    {
+        base.Start();
+        _col = GetComponent<Collider2D>();
+    }
     protected void FixedUpdate()
     {
-        Vector2 vector = rigidBody.velocity;
+        float directionX = Mathf.Sign(_targetDirection.x) * moveSpeed;
+        _velocity.x = directionX;
 
-        if (_ennemyInfo != null && _targetDirection != Vector2.zero)
+
+        _isGrounded = CheckOnGround();
+
+        if (!_isGrounded)
         {
-            vector.x = _targetDirection.x * _ennemyInfo.movementSpeed;
+            _velocity.y += gravity * Time.fixedDeltaTime;
+            if (_velocity.y < maxFallSpeed)
+            {
+                _velocity.y = maxFallSpeed;
+            }
         }
         else
         {
-            vector.x = 0f;
+            _velocity.y = Mathf.Min(_velocity.y, -0.5f);
         }
-        rigidBody.velocity = vector;
+
+        Vector2 mouvement = rigidBody.position + _velocity * Time.fixedDeltaTime;
+        rigidBody.MovePosition(mouvement);
+        Debug.Log(_isGrounded);
     }
 
+
+    private bool CheckOnGround()
+    {
+        var hit = Physics2D.Raycast(transform.position, Vector2.down, groundCheck, LayerMask);
+
+        Debug.DrawRay(transform.position, Vector2.down * .25f);
+        if (hit)
+        {
+            float offset = 0.1f;
+            rigidBody.position = new Vector2(rigidBody.position.x, rigidBody.position.y + offset);
+            return true;
+        }
+
+        return false;
+    }
     public override void Attack()
     {
-        // Not needed yet for base enemy, leave empty or log
-        Debug.Log("Base enemy has no attack yet.");
     }
 
 }
