51476a04aa39b8a08dd6d2e86bc8c963b6d6b435
TalalJD <120325175+TalalJD@users.noreply.github.com>
2025-09-26 10:29:03 -0400

    ajoute de la logique pour le patterne d'attaque en barrage random

--- CodeBreaker/Assets/Scripts/Bosses/Gromar/FSM/AttackPatterns/GS_RandomBarrage.cs
+++ CodeBreaker/Assets/Scripts/Bosses/Gromar/FSM/AttackPatterns/GS_RandomBarrage.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GS_RandomBarrage : MonoBehaviour
+public class GS_RandomBarrage : GromarState
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void OnEnter()
     {
-        
+     
     }
+    public override void OnExit() { }
 
-    // Update is called once per frame
-    void Update()
+
+    public void ShootBulletBarrage()
     {
-        
+        int bulletNumber = 20;
+        if (gromar == null || gromar.bigBullet == null)
+        {
+            Debug.Log("Gromar or bulletPrefab is not assigned");
+            return;
+
+        }
+
+        for (int i = 0; i < bulletNumber; i++)
+        {
+            Vector3 spawnPosition = gromar.GetRandomShootPosition();
+            GameObject bullet = GameObject.Instantiate(gromar.bigBullet, spawnPosition, Quaternion.identity);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                // Always left, with random up/down angle
+                Vector2 dir = new Vector2(-1f, Random.Range(-1f, 1f)).normalized;
+
+                rb.velocity = dir * 5f; // adjust speed
+
+                // Rotate bullet to face movement direction
+                bullet.transform.right = dir;
+            }
+            Debug.Log("omarestgro");
+        }
+
     }
 }
