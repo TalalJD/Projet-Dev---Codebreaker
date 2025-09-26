99ddb867e5625f41b2b6e992c052ea6a4fc50a1f
TalalJD <120325175+TalalJD@users.noreply.github.com>
2025-09-26 10:20:45 -0400

    logique du explostion attack pattern

--- CodeBreaker/Assets/Scripts/Bosses/Gromar/FSM/AttackPatterns/GS_Explostion.cs
+++ CodeBreaker/Assets/Scripts/Bosses/Gromar/FSM/AttackPatterns/GS_Explostion.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GS_Explostion : MonoBehaviour
+public class GS_Explostion : GromarState
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void OnEnter()
     {
-        
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnExit() { }
+    public void ShootExplosion(int bulletCount, float speed)
     {
-        
+        if (gromar == null || gromar.smallBullet == null)
+            return;
+
+        Vector2 center = gromar.transform.position;
+
+        for (int i = 0; i < bulletCount; i++)
+        {
+            // Calculate evenly spaced directions around a circle
+            float angle = i * Mathf.PI * 2f / bulletCount;
+            Vector2 dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)).normalized;
+
+            // Spawn bullet at Gromar's position
+            GameObject bullet = GameObject.Instantiate(gromar.smallBullet, center, Quaternion.identity);
+
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.velocity = dir * speed;
+                bullet.transform.right = dir; // rotate sprite to face movement
+            }
+        }
     }
+
 }
