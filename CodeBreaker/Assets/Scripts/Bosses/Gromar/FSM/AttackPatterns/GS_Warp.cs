f89811000397ed694942e9d2f6afd6cfa88a1967
TalalJD <120325175+TalalJD@users.noreply.github.com>
2025-09-29 12:17:52 -0400

    modification du warp state pour pouvoir prendre en memoir un nombre pour faire plusieur warps

--- CodeBreaker/Assets/Scripts/Bosses/Gromar/FSM/AttackPatterns/GS_Warp.cs
+++ CodeBreaker/Assets/Scripts/Bosses/Gromar/FSM/AttackPatterns/GS_Warp.cs
@@ -8,11 +8,42 @@ public class GS_Warp : GromarState
     SpriteRenderer[] sprites;
     Queue<Transform> WarpHistory = new Queue<Transform>();
     int maxHistory = 3;
+    int warpTimes = 1;
     public override void OnEnter()
     {
         sprites = gromar.GetComponentsInChildren<SpriteRenderer>();
-        WarpPosition();
+        gromar.StartCoroutine(DoWarp());
     }
+
+    /// <summary>
+    /// Permet de set le nombre de fois que le boss va warp
+    /// </summary>
+    /// <param name="count">nombre de warps</param>
+    public GS_Warp(int count)
+    {
+        warpTimes = count;
+    }
+    public GS_Warp()
+    {
+      
+    }
+
+    /// <summary>
+    /// coroutine qui va faire warp le boss avec un delai de 0.3s entre chaque warp
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator DoWarp()
+    {
+        for (int i = 0; i < warpTimes; i++)
+        {
+            WarpPosition();
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+    /// <summary>
+    /// methode qui fait warp le boss avec un point random et qui ajoute ce point dans une liste de memoire 
+    /// </summary>
    public void WarpPosition()
     {
         DisableOrEnableSprites(false);
@@ -34,6 +65,11 @@ public class GS_Warp : GromarState
 
     }
 
+    /// <summary>
+    /// methode qui va regarder la liste de memoir pour verifier que le prochain warp point ne replique pas un pattern ABAB ou qui retourne 
+    /// un point random simple si la liste de memoire n'est pas assez longue pour verifier le patterne
+    /// </summary>
+    /// <returns></returns>
     public Transform GetRandomPointAvoidPattern()
     {
         // Si l'historique est pas assez long on utilise la methode simplifiee
@@ -70,6 +106,10 @@ public class GS_Warp : GromarState
         return possiblePoints[randomIndex];
     }
 
+    /// <summary>
+    /// methdoe qui retourne un point random sur la map parmis la liste des points en evitant de prendre un point ou le boss se trouve deja
+    /// </summary>
+    /// <returns></returns>
     private Transform GetRandomPointSimple()
     {
         var possiblePoints = gromar.mapPoints
@@ -80,7 +120,10 @@ public class GS_Warp : GromarState
         return possiblePoints[randomIndex];
     }
 
-
+    /// <summary>
+    /// methode qui active ou desactive les sprite du boss pour le faire disparaitre visuellement
+    /// </summary>
+    /// <param name="choice"></param>
     public void DisableOrEnableSprites(bool choice)
     {
         foreach (var sprite in sprites)
