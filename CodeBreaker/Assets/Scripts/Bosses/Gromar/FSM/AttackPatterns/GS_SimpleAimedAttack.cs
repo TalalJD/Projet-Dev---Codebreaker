33e1a30fcdcba62ddd6b1f67ac5f8e2ff5ca1e2b
TalalJD <120325175+TalalJD@users.noreply.github.com>
2025-09-29 21:46:28 -0400

    ajout d'un state pour une attaque visee sur le joueur simple

--- CodeBreaker/Assets/Scripts/Bosses/Gromar/FSM/AttackPatterns/GS_SimpleAimedAttack.cs
+++ CodeBreaker/Assets/Scripts/Bosses/Gromar/FSM/AttackPatterns/GS_SimpleAimedAttack.cs
@@ -7,19 +7,19 @@ public class GS_SimpleAimedAttack : GromarState
 
     public override void OnEnter()
     {
-        gromar.StartCoroutine(ShootAtPlayerContinuously(10f, 1f));
+        gromar.StartCoroutine(ShootAtPlayerContinuously(10f, .1f));
     }
     public override void OnExit() { }
 
     public IEnumerator ShootAtPlayerContinuously(float speed, float delay)
     {
-        for(int i = 0; i < 10; i++) 
+        for(int i = 0; i < 100; i++) 
         {
             Vector2 playerTarget = (Vector2)gromar.player.transform.position + new Vector2(0, 0.5f);
             Vector2 dir = (playerTarget - (Vector2)gromar.ShootingPoint.position).normalized;
 
             // centralized bullet spawning
-            gromar.ShootMissileBullet(gromar.ShootingPoint.position, gromar.player.transform.position);
+            gromar.ShootSmallBullet(gromar.ShootingPoint.position, dir, 10f);
 
             yield return new WaitForSeconds(delay);
         }
