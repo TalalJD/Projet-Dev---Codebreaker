c937d88f03f06b11e2ac535d18a3be231a384203
TalalJD <120325175+TalalJD@users.noreply.github.com>
2025-10-17 14:22:14 -0400

    updates meta.cs

---
+++ CodeBreaker/Assets/Scripts/Projectiles/BasePlayerBullet.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  BasePlayerBullet : MonoBehaviour
+{
+    [SerializeField] private WeaponInfo _weaponInfo;
+    private float _speed;
+    private float _lifeTime;
+    private float _damage;
+    private Rigidbody2D rb;
+    protected virtual void Start()
+    {
+        _speed = _weaponInfo.bulletSpeed;
+        _lifeTime = _weaponInfo.bulletLifeTime;
+        _damage = _weaponInfo.damage;
+
+        rb = GetComponent<Rigidbody2D>();
+        if (rb == null)
+            Debug.LogError("Bullet requires a Rigidbody2D on the same GameObject!", this);
+
+        Destroy(gameObject, _lifeTime);
+    }
+
+
+    protected virtual void FixedUpdate()
+    {
+        transform.Translate(Vector3.up * _speed * Time.fixedDeltaTime, Space.Self);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ennemy"))
+        {
+           
+            Destroy(gameObject, .5f);
+        }
+    }
+}
---
+++ CodeBreaker/Assets/Scripts/Projectiles/EnnemyBullet.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Loading;
+using UnityEngine;
+
+public class EnnemyBullet : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag != "Player" && collision.tag != "Ennemy" && collision.tag != "Boss" && collision.tag != "EnnemyBullet")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
