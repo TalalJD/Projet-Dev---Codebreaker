6c7bbc3d9de74cae13944f41faa54868d6c7354d
YaniAgoune <ravless21@gmail.com>
2025-10-24 10:09:41 -0400

    Ajout de la logique de récupération de consommables dans AddItemToInventory

--- CodeBreaker/Assets/Scripts/Player/Player.cs
+++ CodeBreaker/Assets/Scripts/Player/Player.cs
@@ -30,12 +30,15 @@ public class Player : MonoBehaviour
     public WeaponInfo SelectedWeaponInfo;//info de l'arme selectionne
     public List<ScriptableObject> WeaponInventory; //liste des armes scriptable object dans l'inventaire
     public List<ScriptableObject> ConsumableInventory; //liste des consomable scriptable object dans l'inventaire
+    public Consumable SelectedConsumable;
+    public ConsumableInfo SelectedConsumableInfo;
     public Transform WeaponHolder;
     private int inventoryIndex = -1; //index de l'item selectionee dans l'inventaire
+    private int ConsumableInventoryIndex = -1;
 
 
     public event Action OnWeaponInventoryChanged;
-    //public event Action OnConsInventoryChanged;
+    public event Action OnConsInventoryChanged;
     public event Action<int, int> OnHealthChanged;
    
 
@@ -171,7 +174,7 @@ public class Player : MonoBehaviour
     /// Methode qui regarde si l'item est une arme ou un objet et la rajoute dans la liste d'inventaire specifique du joueur
     /// </summary>
     /// <param name="item">scriptable object recupere</param>
-   public void AddItemToInventory(ScriptableObject item)
+    public void AddItemToInventory(ScriptableObject item)
     {
         if(item.GetType() == typeof(WeaponInfo) && WeaponInventory != null)
         {
@@ -199,6 +202,32 @@ public class Player : MonoBehaviour
 
             }
             OnWeaponInventoryChanged?.Invoke();
+        }
+
+        if (item.GetType() == typeof(ConsumableInfo) && ConsumableInventory != null)
+        {
+            if (ConsumableInventory.Count < 2)
+            {
+                ConsumableInventory.Add(item);
+            }
+            else
+            {
+                    if (SelectedConsumable != null)
+                    {
+                        int selectedIndex = ConsumableInventory.IndexOf(SelectedConsumableInfo);
+                        if (selectedIndex != -1)
+                        {
+                            ConsumableInventory[selectedIndex] = item;
+                            EquipConsumable(selectedIndex);
+                        }
+                }
+                else
+                {
+                    ConsumableInventory[0] = item;
+                    EquipConsumable(0);
+                }
+            }
+            OnConsInventoryChanged?.Invoke();
         }
 
     }
