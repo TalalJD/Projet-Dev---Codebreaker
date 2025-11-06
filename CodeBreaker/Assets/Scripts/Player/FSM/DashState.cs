6f64d280bae07a91926d842fe5f5d3594e0b35de
cyrill786 <160172211+cyrill786@users.noreply.github.com>
2025-11-05 19:52:38 -0500

    Dash dans les airs+fix quelques bugs

--- CodeBreaker/Assets/Scripts/Player/FSM/DashState.cs
+++ CodeBreaker/Assets/Scripts/Player/FSM/DashState.cs
@@ -7,18 +7,40 @@ public class DashState : PlayerState
     private float dashTimer;
     private float dashDuration = 0.2f;
     private float dashSpeed = 15f;
+    private int dashDirection; // -1 ou 1 / gauche ou droite
     private bool isDashing;
-    private bool dashEnd;
+
+    private static float dashCooldown = 5f;
+    private static float dashCurrentCooldown = 0f;
+
 
     public DashState() : base(3){ }
 
     public override void OnEnter()
     {
+        // Gauche ou droite
+        float inputX = Input.GetAxisRaw("Horizontal");
+
+        // Utilise le valeur input pour le dashDirection
+        if (inputX != 0f)
+        {
+            dashDirection = (int)Mathf.Sign(inputX);
+        } else
+        {
+            // Garder la même direction
+            dashDirection = (int)Mathf.Sign(Player.Direction == 0 ? 1 : Player.Direction);
+        }
+
         isDashing = true;
         dashTimer = dashDuration;
 
-        Player.Rb.linearVelocity = new Vector2(Player.Direction * dashSpeed, 0);
+        // Permet le dash Horizontal
+        Player.Rb.linearVelocity = new Vector2(Player.GroundSpeed, Player.Rb.linearVelocity.y);
         Player.YSpeed = 0;
+
+
+        // Commencer le timer
+        dashCurrentCooldown = dashCooldown;
 
         Debug.Log("Dash started");
     }
@@ -26,17 +48,28 @@ public class DashState : PlayerState
     public override void OnExit()
     {
         isDashing = false;
-        Debug.Log("Dash ended");
+
+        // Débarassée de movement Horizontal de Dash pour retourner au move state
+        Player.GroundSpeed = 0f;
+        Player.Rb.linearVelocity = new Vector2(0f, Player.Rb.linearVelocity.y);
     }
 
     public override void OnUpdate()
     {
         SetDirection(Player.Direction);
 
-        if (Input.GetKeyDown(KeyCode.B) && !isDashing)
+        
+        if (dashCurrentCooldown > 0)
+            dashCurrentCooldown -= Time.deltaTime;
+
+        
+        if (Input.GetKeyDown(KeyCode.B) && !isDashing && dashCurrentCooldown <= 0)
         {
+            
             Machine.Set<DashState>();
         }
+
+
     }
 
     public override void OnFixedUpdate()
