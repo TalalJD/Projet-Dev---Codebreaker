6fb38f37cbe501c8584d0df3b84f13f68758e6c1
YaniAgoune <ravless21@gmail.com>
2025-11-06 08:42:51 -0500

    Modification du comportement de l'ennemi volant

--- CodeBreaker/Assets/Scripts/Ennemy/StaticEnnemy.cs
+++ CodeBreaker/Assets/Scripts/Ennemy/StaticEnnemy.cs
@@ -9,6 +9,12 @@ public class StaticEnnemy : Ennemy
     [SerializeField] private float projectileSpeed = 12f;
     [SerializeField] private float attackRange = 10f;
 
+    [SerializeField] private Vector2 followOffset = new Vector2(2f, 4f);
+    [SerializeField] private float moveSpeed = 4f;
+    [SerializeField] private float maxY = 0f;
+    protected float distanceTarget;
+    private Rigidbody2D _rb;
+
     protected override void Start()
     {
         base.Start();
@@ -16,19 +22,51 @@ public class StaticEnnemy : Ennemy
         {
             firePoint = transform;
         }
+        _rb = GetComponent<Rigidbody2D>();
+        ResetCooldown();
     }
-    protected override void Update()
+    private void FixedUpdate()
     {
-        base.Update();
-        if (CanAttack() && _targetDirection.magnitude <= attackRange)
+        if (_target != null)
         {
-            Attack();
+            distanceTarget = Vector2.Distance(transform.position, _target.position);
+        }
+        else
+        {
+            distanceTarget = Mathf.Infinity;
+        }
+
+        if (distanceTarget <= attackRange)
+        {
+            if (CanAttack())
+            {
+                RegarderJoueur(_targetDirection.x);
+                Attack();
+            }
+            else
+            {
+                Move();
+            }
         }
     }
 
+    private void Move()
+    {
+        Vector2 direction = (Vector2)_target.position + followOffset;
+
+        direction.y = Mathf.Min(direction.y, maxY);
+
+
+        Vector2 newPos = Vector2.MoveTowards(
+            _rb.position,
+            direction,
+            moveSpeed * Time.fixedDeltaTime
+        );
+
+        _rb.MovePosition(newPos);
+    }
     public override void Attack()
     {
-
         GameObject projectile = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
         Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
         if (rb != null)
