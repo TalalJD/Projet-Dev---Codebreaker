82a888f7c5817ed8692f61d87e60bfdd5a2f73a8
TalalJD <2277175@bdeb.qc.ca>
2025-12-01 09:56:43 -0500

    Death state et anim

--- CodeBreaker/Assets/Scripts/Player/FSM/DeathState.cs
+++ CodeBreaker/Assets/Scripts/Player/FSM/DeathState.cs
@@ -3,11 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Player death state:
-/// - StateNumber = 5
-/// - 2s timer, then fires Player.OnDeath event
-/// </summary>
+
 public class DeathState : PlayerState
 {
     public DeathState() : base(5) { }
@@ -18,16 +14,18 @@ public class DeathState : PlayerState
     {
         timer = 2f;
 
-        if(Player != null)
+        if (Player != null)
         {
+           
+            Player.GroundSpeed = 0f;
+
             if (Player.Rb != null)
             {
-                Player.Rb.linearVelocity = Vector2.zero;
                 Player.Rb.angularVelocity = 0f;
             }
 
             Player.canTakeDmg = false;
-            Player.IsBlocking = true; 
+            Player.IsBlocking = true;
 
             Player.ClearHeldItem();
 
@@ -41,13 +39,34 @@ public class DeathState : PlayerState
     public override void OnFixedUpdate()
     {
         timer -= Time.fixedDeltaTime;
+
+   
+        Player.YSpeed -= PhysicsInfo.Gravity * Time.fixedDeltaTime;
+
+        if (Player.YSpeed < -PhysicsInfo.MaxFallSpeed)
+        {
+            Player.YSpeed = -PhysicsInfo.MaxFallSpeed;
+        }
+
+        if (Player.CheckOnGround() && Player.YSpeed < 0)
+        {
+            Player.YSpeed = 0f;
+        }
+
+        
+        if (Player.Rb != null)
+        {
+            Player.Rb.linearVelocity = new Vector2(0f, Player.YSpeed);
+        }
+
         if (timer <= 0f)
         {
+          
             Player.TriggerDeathEvent();
         }
     }
 
-    public override void OnExit() {}
+    public override void OnExit() { }
 
     public override void OnUpdate() { }
 }
